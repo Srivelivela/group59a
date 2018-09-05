07ad252a0bd34a81189a2c04094acd8fbb2ae272
Ryan Gadhi <gadi@gatech.edu>
2018-09-05 14:31:49 -0400

    updated person3

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -1,25 +1,64 @@
 package edu.gatech.oad.antlab.person;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
- *  A simple class for person 3
+ *  A simple class for person 2
  *  returns their name and a
- *  reversed string 
- *  
- *  @author  Bob
- *  @version 1.1
+ *  modified string 
+ *
+ * @author RyanGadhi
+ * @version 1.1
  */
 public class Person3 {
-   /** Holds the persons real name */  
-	private String name;
-	
-	/**
+    /** Holds the persons real name */
+    private String name;
+	 	/**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
 	 */
-	public Person3(String pname){
-	  name = pname;
+	 public Person3(String pname) {
+	   name = pname;
+	 }
+	/**
+	 * This method should take the string
+	 * input and return its characters in
+	 * random order.
+	 * given "gtg123b" it should return
+	 * something like "g3tb1g2".
+	 *
+	 * @param input the string to be modified
+	 * @return the modified string
+	 */
+	private String calc(String input) {
+	  //Person 2 put your implementation here
+
+		String output = null;
+		char[] arr = input.toCharArray();
+		//int random = (int)(10*Math.random());
+		Random ran = new Random();
+
+
+		char[] arr2 = new char[arr.length];
+		ArrayList<Integer> usedIndex = new ArrayList<Integer>();
+		int currentRan=-1;
+
+		for(int i=0 ; i< arr.length ; i++){
+
+			while(usedIndex.contains(currentRan))
+			 currentRan = ran.nextInt(arr.length);
+
+			usedIndex.add(currentRan);
+			arr2[i]=arr[currentRan]; //nnnnnn
+
+		}
+		output = arr2.toString();
+
+
+		return null;
 	}
-	
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
@@ -30,19 +69,5 @@ public class Person3 {
 	 */
 	public String toString(String input) {
 	  return name + calc(input);
-	}
-	
-	/**
-	 * This method should take the string
-	 * input and return its reverse.
-	 * given "gtg123b" it should return
-	 * b321gtg.
-	 *
-	 * @param input the string to be reversed
-	 * @return the reversed string
-	 */
-	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
 	}
 }
