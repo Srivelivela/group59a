dc00a654ec64588c6dc4dcdd34cb9c28bf66d360
Ryan Gadhi <gadi@gatech.edu>
2018-09-06 22:10:37 -0400

    update person 3

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,11 +1,14 @@
 package edu.gatech.oad.antlab.person;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *  A simple class for person 2
  *  returns their name and a
  *  modified string 
  *
- * @author Bob
+ * @author RyanGadhi
  * @version 1.1
  */
 public class Person2 {
@@ -31,7 +34,30 @@ public class Person2 {
 	 */
 	private String calc(String input) {
 	  //Person 2 put your implementation here
-	  return null;
+
+		String output = null;
+		char[] arr = input.toCharArray();
+		//int random = (int)(10*Math.random());
+		Random ran = new Random();
+
+
+		char[] arr2 = new char[arr.length];
+		ArrayList<Integer> usedIndex = new ArrayList<Integer>();
+		int currentRan=-1;
+
+		for(int i=0 ; i< arr.length ; i++){
+
+			while(usedIndex.contains(currentRan))
+			 currentRan = ran.nextInt(arr.length);
+
+			usedIndex.add(currentRan);
+			arr2[i]=arr[currentRan]; //it is working
+
+		}
+		output = arr2.toString();
+
+
+		return null;
 	}
 	/**
 	 * Return a string rep of this object
